4bafcd4c4a1035be296b4c75b6662eab6b7bb074
dfdfdfd123 <winwin4668@naver.com>
2025-02-12 14:13:39 +0900

    test

--- src/main/java/bitc/fullstack503/e2teamproject/repository/BoardRepository.java
+++ src/main/java/bitc/fullstack503/e2teamproject/repository/BoardRepository.java
@@ -1,4 +1,13 @@
 package bitc.fullstack503.e2teamproject.repository;
 
-public interface BoardRepository {
+import bitc.fullstack503.e2teamproject.entity.BoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
+
+    @Query("SELECT b FROM BoardEntity b ORDER BY b.board_idx DESC")
+    List<BoardEntity> findAllByOrderByBoard_idxDesc();
 }
