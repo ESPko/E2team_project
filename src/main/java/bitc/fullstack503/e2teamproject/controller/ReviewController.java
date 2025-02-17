ec1581fa7836978ae7666df45402b0b8d7d6c679
void-alive <sja9284@gmail.com>
2025-02-17 15:42:48 +0900

    review 별점 남기기 기능 실험중

--- src/main/java/bitc/fullstack503/e2teamproject/controller/ReviewController.java
+++ src/main/java/bitc/fullstack503/e2teamproject/controller/ReviewController.java
@@ -4,11 +4,12 @@ import bitc.fullstack503.e2teamproject.entity.ReviewEntity;
 import bitc.fullstack503.e2teamproject.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+@RestController
 @Controller
 @RequestMapping("/review")
 public class ReviewController {
@@ -20,5 +21,14 @@ public class ReviewController {
   public ModelAndView jiHyunStarTest(){
     ModelAndView mav = new ModelAndView("/board/jiHyunStarTest");
     return mav;
+  }
+
+  @ResponseBody
+  @RequestMapping("/write/{reviewWrite}/{reviewStar}")
+  public void reviewWrite(@PathVariable("reviewWrite") String reviewWrite,
+                          @PathVariable("reviewStar") double reviewStar){
+    System.out.println("reviewWrite");
+    System.out.println(reviewWrite);
+    System.out.println(reviewStar);
   }
 }
