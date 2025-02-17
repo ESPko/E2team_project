0d9e83ed8e7d0987cc26ebd549e352a9f9c94ce1
void-alive <sja9284@gmail.com>
2025-02-17 12:50:59 +0900

    지역, 인원수, 나이 별로 나온 내용을 별점에 따라 정렬하는 기능 구현중

--- src/main/java/bitc/fullstack503/e2teamproject/controller/PlaceController.java
+++ src/main/java/bitc/fullstack503/e2teamproject/controller/PlaceController.java
@@ -29,15 +29,31 @@ public class PlaceController {
   @ResponseBody
   @GetMapping("/recommend/{selectLocation}/{selectAge}/{selectPeople}")
   public List<PlaceEntity> recommendPlace(@PathVariable("selectLocation") String selectLocation,
-                             @PathVariable("selectAge") int selectAge,
-                             @PathVariable("selectPeople") int selectPeople) {
+                                          @PathVariable("selectAge") int selectAge,
+                                          @PathVariable("selectPeople") int selectPeople) {
     return placeService.recommendPlace(selectLocation, selectAge, selectPeople);
   }
 
+  //  선택한 지역, 나이, 사람 수와 리뷰가 높은 순으로 출력
   @ResponseBody
-  @RequestMapping("/recommend/result")
-  public void recommendResult(){
+  @RequestMapping("/recommend/starHigh/{selectLocation}/{selectAge}/{selectPeople}")
+  public List<PlaceDTO> starHigh(@PathVariable("selectLocation") String selectLocation,
+                       @PathVariable("selectAge") int selectAge,
+                       @PathVariable("selectPeople") int selectPeople) {
+    System.out.println("추천수 높은 순");
+    System.out.println(placeService.findPlaceStarHigh(selectLocation,selectAge,selectPeople));
+    return placeService.findPlaceStarHigh(selectLocation, selectAge, selectPeople);
+  }
 
+  //  선택한 지역, 나이, 사람 수와 리뷰가 낮은 순으로 출력
+  @ResponseBody
+  @RequestMapping("/recommend/starLow/{selectLocation}/{selectAge}/{selectPeople}")
+  public List<PlaceDTO> starRow(@PathVariable("selectLocation") String selectLocation,
+                      @PathVariable("selectAge") int selectAge,
+                      @PathVariable("selectPeople") int selectPeople) {
+    System.out.println("추천수 낮은 순");
+    System.out.println(placeService.findPlaceStarLow(selectLocation,selectAge,selectPeople));
+    return placeService.findPlaceStarLow(selectLocation, selectAge, selectPeople);
   }
 
   //  상위 세개 추천 항목 나오게
