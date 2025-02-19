ef47cc772b9dfae47bd8e0f750aa40df4e2d9661
void-alive <sja9284@gmail.com>
2025-02-19 11:05:52 +0900

    db 컬럼 수정

--- src/main/java/bitc/fullstack503/e2teamproject/controller/PlaceImageController.java
+++ src/main/java/bitc/fullstack503/e2teamproject/controller/PlaceImageController.java
@@ -1,10 +1,13 @@
 package bitc.fullstack503.e2teamproject.controller;
 
+import bitc.fullstack503.e2teamproject.entity.PlaceImageEntity;
 import bitc.fullstack503.e2teamproject.service.PlaceImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/placeImage")
@@ -13,9 +16,17 @@ public class PlaceImageController {
   @Autowired
   private PlaceImageService placeImageService;
 
+  //  뷰만
   @RequestMapping("/")
   public ModelAndView placeImage() {
-    ModelAndView mav = new ModelAndView("jiHyunStarTest");
-    return mav;
+    return new ModelAndView("/board/jiHyunImageTest");
+  }
+
+//  전부 다 확인
+  @RequestMapping("/findAll")
+  public List<PlaceImageEntity> findAll(){
+    List<PlaceImageEntity> findImageAll= placeImageService.findPlaceAll();
+    System.out.println(findImageAll);
+    return findImageAll;
   }
 }
