c8d06030a2cdf6b7b6ad2b4324539046c42e2087
void-alive <sja9284@gmail.com>
2025-02-20 12:35:22 +0900

    리뷰작성 폼 틀 작성

--- src/main/java/bitc/fullstack503/e2teamproject/service/ReviewServiceImpl.java
+++ src/main/java/bitc/fullstack503/e2teamproject/service/ReviewServiceImpl.java
@@ -6,6 +6,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 
@@ -15,33 +16,28 @@ public class ReviewServiceImpl implements ReviewService {
   private final ReviewRepository reviewRepository;
 
 
-//  해당 게시물에서 작성한 리뷰 테스트
+  //  해당 게시물에서 작성한 리뷰 테스트
   @Override
   public List<ReviewEntity> getReviewsByPlace(int placeIdx) {
-
     return reviewRepository.findByPlaceReview_PlaceIdxOrderByStarDesc(placeIdx);
-
-
   }
 
-//   리뷰 삭제 테스트
+  //   리뷰 삭제 테스트
   @Override
   @Transactional
   public void deleteReview(int reviewIdx) {
     reviewRepository.deleteById(reviewIdx);
   }
 
-// 내가 작성한 리뷰
+  // 내가 작성한 리뷰
   @Override
   public List<ReviewEntity> findReviewsByUserId(int userId) {
-
     return reviewRepository.findReviewsByUserId(userId);
   }
 
   @Override
-  public void reviewStar(String reviewComment, double reviewStar){
+  public void reviewStar(String reviewComment, double reviewStar) {
     reviewRepository.queryWriteReview(reviewComment, reviewStar);
   }
-
 }
 
