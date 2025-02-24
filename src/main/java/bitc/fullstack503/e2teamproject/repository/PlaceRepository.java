1233ab78e3bc1ca0b659e280acfc3503061eb91c
void-alive <sja9284@gmail.com>
2025-02-24 11:03:24 +0900

    place service, repository 수정

--- src/main/java/bitc/fullstack503/e2teamproject/repository/PlaceRepository.java
+++ src/main/java/bitc/fullstack503/e2teamproject/repository/PlaceRepository.java
@@ -25,18 +25,6 @@ public interface PlaceRepository extends JpaRepository<PlaceEntity, Integer> {
   @Query("select p from PlaceEntity p where p.placeIdx = :placeIdx")
   List<PlaceEntity> queryFindPlace(@Param("placeIdx") int placeIdx);
 
-  //  볼링장 카테고리 가져오기
-  @Query(value = "SELECT * FROM place WHERE category = '볼링장' LIMIT 1", nativeQuery = true)
-  PlaceEntity queryFindCategoryBalling();
-
-  //  클라이밍 카테고리 가져오기
-  @Query(value = "SELECT * FROM place WHERE category = '클라이밍' LIMIT 1", nativeQuery = true)
-  PlaceEntity queryFindCategoryClimbing();
-
-  //  코인노래방 카테고리 가져오기
-  @Query(value = "SELECT * FROM place WHERE category = '코인노래방' LIMIT 1", nativeQuery = true)
-  PlaceEntity queryFindCategoryCoinSing();
-
   //  공방 카테고리 가져오기
   @Query(value = "SELECT * FROM place WHERE category = '공방' LIMIT 1", nativeQuery = true)
   PlaceEntity queryFindCategoryGold();
@@ -45,7 +33,107 @@ public interface PlaceRepository extends JpaRepository<PlaceEntity, Integer> {
   @Query(value = "SELECT * FROM place WHERE category = '댄스학원' LIMIT 1", nativeQuery = true)
   PlaceEntity queryFindCategoryDance();
 
-//  카트체험 카테고리 가져오기
+  //  도서관 카테고리 가져오기
+  @Query(value = "select * from place where category='도서관' limit 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryLibrary();
+
+  //  동물카페
+  @Query(value = "select * from place where category='동물카페' limit 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryAnimal();
+
+  //  루지
+  @Query(value = "select * from place where category='루지' limit 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryLoogi();
+
+  //  멀티방
+  @Query(value = "select * from place where category='멀티방' limit 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryMulti();
+
+  //  메이드집사
+  @Query(value = "select * from place where category='메이드집사' limit 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryMaid();
+
+  //  무인스튜디오
+  @Query(value = "select * from place where category='무인스튜디오' limit 1", nativeQuery = true)
+  PlaceEntity queryFindCategorySelfStudio();
+
+  //  방탈출
+  @Query(value = "select * from place where category='방탈출' limit 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryEscapeRoom();
+
+  //  베이킹
+  @Query(value = "select * from place where category='베이킹체험' limit 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryBaking();
+
+  //  보드게임
+  @Query(value = "select * from place where category='보드게임' limit 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryBoard();
+
+  //  볼링장 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '볼링장' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryBalling();
+
+  //  서바이벌 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '서바이벌' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategorySurvival();
+
+  //  수상레저 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '수상레저' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryWater();
+
+//  스카이다이빙
+  @Query(value = "select * from place where category='스카이다이빙'", nativeQuery = true)
+  PlaceEntity queryFindCategorySkyDiving();
+
+  //  슬라임카페 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '슬라임카페' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategorySlime();
+
+  //  실내사격 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '실내사격' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryShooting();
+
+  //  짚라인 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '짚라인' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryZipLine();
+
+  //  카트체험 카테고리 가져오기
   @Query(value = "SELECT * FROM place WHERE category = '카트장'", nativeQuery = true)
   PlaceEntity queryFindCategoryCart();
+
+  //  컨벤션 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '컨벤션'", nativeQuery = true)
+  PlaceEntity queryFindCategoryConvention();
+
+  //  코인노래방 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '코인노래방' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryCoinSing();
+
+  //  클라이밍 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '클라이밍' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryClimbing();
+
+  //  키자니아 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '키자니아' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryKizania();
+
+  //  테마파크 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '테마파크' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryThemePark();
+
+//  틱톡
+  @Query(value = "select * from place where category='틱톡부스'", nativeQuery = true)
+  PlaceEntity queryFindCategoryTikTok();
+
+  //  파티룸 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '파티룸' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryPartyRoom();
+
+//  페러세일
+  @Query(value = "select * from place where category = '페러세일링'", nativeQuery = true)
+  PlaceEntity queryFindCategoryParasail();
+
+  //  홀덤펍 카테고리 가져오기
+  @Query(value = "SELECT * FROM place WHERE category = '홀덤펍' LIMIT 1", nativeQuery = true)
+  PlaceEntity queryFindCategoryHoldum();
 }
