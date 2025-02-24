215713f5a260b21e34cb31f51912d190fb6cbcb6
void-alive <sja9284@gmail.com>
2025-02-24 11:03:24 +0900

    place service, repository 수정

--- src/main/java/bitc/fullstack503/e2teamproject/service/PlaceService.java
+++ src/main/java/bitc/fullstack503/e2teamproject/service/PlaceService.java
@@ -15,11 +15,41 @@ public interface PlaceService {
   //  해당 placeIdx 의 정보 가져오기
   List<PlaceEntity> findPlaceDetail(@RequestParam("placeIdx") int placeIdx);
 
+  //  도서관
+  PlaceEntity findPlaceLibrary();
+
+  //  동물카페
+  PlaceEntity findPlaceAnimal();
+
+  //  루지
+  PlaceEntity findPlaceLoogi();
+
+  //  멀티방
+  PlaceEntity findPlaceMulti();
+
+  //  메이드집사
+  PlaceEntity findPlaceMaid();
+
+  //  무인스튜디오
+  PlaceEntity findPlaceSelfStudio();
+
+  //  방탈출
+  PlaceEntity findPlaceEscapeRoom();
+
+  //  베이킹
+  PlaceEntity findPlaceBaking();
+
+  //  보드게임
+  PlaceEntity findPlaceBoard();
+
   //  볼링장 카테고리
   PlaceEntity findPlaceBalling();
 
   //  클라이밍
   PlaceEntity findPlaceClimbing();
+
+  //  컨벤션
+  PlaceEntity findPlaceConvention();
 
   //  코인노래방
   PlaceEntity findPlaceCoinSing();
@@ -30,6 +60,42 @@ public interface PlaceService {
   //  댄스학원
   PlaceEntity findPlaceDance();
 
+  //  서바이벌
+  PlaceEntity findPlaceSurvival();
+
+  //  수상레저
+  PlaceEntity findPlaceWater();
+
+  //  스카이다이빙
+  PlaceEntity findPlaceSkyDiving();
+
+  //  슬라임카페
+  PlaceEntity findPlaceSlime();
+
+  //  실내사격
+  PlaceEntity findPlaceShooting();
+
+  //  짚라인
+  PlaceEntity findPlaceZipLine();
+
   //  카트체험
   PlaceEntity findPlaceCart();
+
+  //  키자니아
+  PlaceEntity findPlaceKizania();
+
+  //테마파크
+  PlaceEntity findPlaceThemePark();
+
+  //  틱톡
+  PlaceEntity findPlaceTikTok();
+
+  //  파티룸
+  PlaceEntity queryFindCategoryPartyRoom();
+
+  //  페러세일
+  PlaceEntity queryFindCategoryParasail();
+
+  //  홀덤펍
+  PlaceEntity queryFindCategoryHoldum();
 }
